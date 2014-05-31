c81a9b2361f9a97c260f912294b3660d3038e6e4
Evan Hadfield <evanhadfield@gmail.com>
2014-05-31 13:43:33 -0500

    scaffold Donor and Recipient

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140531184309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "donors", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.boolean  "organization"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipients", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.boolean  "organization"
+    t.string   "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
