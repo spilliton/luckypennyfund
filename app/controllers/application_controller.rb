4cdd9a8e4399ff8b4cf2e234dec4ddb7a5c73161
Zachary Kloepping <zachary@massrelevance.com>
2014-05-31 18:14:23 -0500

    locking down users controller to superusers only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def require_superuser
+    unless current_user && current_user.superuser?
+      redirect_to "/"
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  before_action :require_superuser
+
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+
 
   # GET /users
   # GET /users.json
