35c0adeb4dca5fd89a28976d541c198f3c9e5e8b
simonlee <lees1183@gmail.com>
2014-05-31 17:40:12 -0500

    sanitize works for user edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :address1, :address2, :locality, :state, :zip, :country, :gender, :dob) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :address1, :address2, :locality, :state, :zip, :country, :gender, :dob) }
   end
 end
